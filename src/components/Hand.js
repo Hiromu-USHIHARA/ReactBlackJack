30f4b5504e7f3fdaa25ddd83c67a681f22b8d505
Hiromu Ushihara <h.ushihara223@gmail.com>
2025-04-18 21:06:09 +0900

    Update UI: add 'wrap'

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -1,11 +1,11 @@
 import Card from "./Card"
-import {Group, Title} from "@mantine/core"
+import { Group, Title} from "@mantine/core"
 
 export default function Hand({cards, title}){
     return (
         <div style={{marginBottom: "20px"}}>
             <Title order={4} align="center">{title}</Title>
-            <Group spacing="xs">
+            <Group spacing="xs" position="center" wrap="wrap">
                 {cards.map((card, index) => (
                     <Card key={index} suit={card.suit} value={card.value} />
                 ))}
